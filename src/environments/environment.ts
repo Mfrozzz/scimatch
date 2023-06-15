a98836d6ee2116fcd8ccb364ced6e667f1656304
“Mfrozzz” <marcosvinibva@gmail.com>
2023-06-15 14:58:09 -0300

    firebase inicial

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'scimatch-a3481',
+    appId: '1:111903485152:web:d0b9feadb873d0e288b485',
+    storageBucket: 'scimatch-a3481.appspot.com',
+    apiKey: 'AIzaSyBkIUm2c1onCL6kxoH2Gn-gGg1lMrVN7ts',
+    authDomain: 'scimatch-a3481.firebaseapp.com',
+    messagingSenderId: '111903485152',
+  },
+};
