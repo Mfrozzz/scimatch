0098301159532442d2b5cf488fb1f54ad05ccb59
“Mfrozzz” <marcosvinibva@gmail.com>
2023-06-28 17:52:09 -0300

    inicio login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,15 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { provideStorage,getStorage } from '@angular/fire/storage';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
