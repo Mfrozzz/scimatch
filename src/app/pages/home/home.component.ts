b38d45611dc7852b6037c4f243a9184f6149ae22
“Mfrozzz” <marcosvinibva@gmail.com>
2023-07-05 23:51:50 -0300

    pequenas coisas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+
 }
+
