f245e04938ce1d7976c1fa8286f4d0e633fbfc35
“Mfrozzz” <marcosvinibva@gmail.com>
2023-07-27 09:48:16 -0300

    create - update project front

--- src/app/pages/user/project/create-project/create-project.component.ts
+++ src/app/pages/user/project/create-project/create-project.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-project',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent {
+  tiposDoc: string[]=["TCC","Extensão","Artigo","Relatório","Monografia","Tese","Dissertação"];
+  projectForm!:FormGroup;
+  document:any;
+  isSubmitted: boolean = false;
 
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(){
+    this.projectForm = this.formBuilder.group({
+      name: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      type: ["",[Validators.required]],
+      docURL: ["",[Validators.required]]
+    })
+  }
+
+  submitForm(){
+    this.isSubmitted = true;
+    if(!this.projectForm.valid){
+      console.log("aqui");
+    }else{
+      this.edit();
+    }
+  }
+
+  uploadFile(document:any){
+    this.document = document.files;
+  }
+
+  edit(){
+    
+  }
 }
--- src/app/pages/user/project/update-project/update-project.component.ts
+++ src/app/pages/user/project/update-project/update-project.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-update-project',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-project.component.css']
 })
 export class UpdateProjectComponent {
+  tiposDoc: string[]=["TCC","Extensão","Artigo","Relatório","Monografia","Tese","Dissertação"];
+  projectForm!:FormGroup;
+  document:any;
+  isSubmitted: boolean = false;
 
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(){
+    this.projectForm = this.formBuilder.group({
+      name: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      type: ["",[Validators.required]],
+      docURL: ["",[Validators.required]]
+    })
+  }
+
+  submitForm(){
+    this.isSubmitted = true;
+    if(!this.projectForm.valid){
+      console.log("aqui");
+    }else{
+      this.edit();
+    }
+  }
+
+  uploadFile(document:any){
+    this.document = document.files;
+  }
+
+  edit(){
+    
+  }
 }
